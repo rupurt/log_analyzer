3abc754d82fe59c5726155fa3438da7703c52942
Alex Kwiatkowski <alex+git@rival-studios.com>
2017-03-07 09:19:16 -0800

    Add import to log_analyzer CLI

--- exe/insert.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-
-`gunzip -c /Users/alex/Desktop/tiny-sk-logs/* | sed 's/\\./\\\\\./g' | psql -d log_analyzer -c "COPY logs (row, without_zone, with_zone, not_sure, application_name, ip, local7, info, dyno, data) FROM stdin"`
--- lib/log_analyzer/cli.rb
+++ lib/log_analyzer/cli.rb
@@ -16,5 +16,19 @@ module LogAnalyzer
       now = Time.now.strftime(TIME_FORMAT)
       template "db/migrate/create_logs.rb", "db/migrate/#{now}_create_logs.rb"
     end
+
+    desc "import file table", "import a log file into a table"
+    def import(file, table)
+      StandaloneMigrations::Configurator.load_configurations
+      database = ActiveRecord::Base.configurations[Rails.env]["database"]
+
+      pipeline = [
+        "cat #{file}",
+        "sed 's/\\\\./\\\\\\\\./g'",
+        "psql -d #{database} -c 'COPY #{table} (row, without_zone, with_zone, not_sure, application_name, ip, local7, info, dyno, data) FROM stdin'"
+      ]
+
+      `#{pipeline.join(" | ")}`
+    end
   end
 end
